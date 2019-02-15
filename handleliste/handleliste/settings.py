069fe17cfd2078f2ba663e42a8c985c0762ad450
haakofli <hakon.fuhre.lia@gmail.com>
2019-02-15 13:30:05 +0100

    html textfield, button and dummydata for create new list

--- handleliste/handleliste/settings.py
+++ handleliste/handleliste/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'users.apps.UsersConfig',
+    'crispy_forms',
     'liste.apps.ListeConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
--- handleliste/liste/models.py
+++ handleliste/liste/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+
+class CreateNewList():
+    title = models.CharField(max_length=100)
+
