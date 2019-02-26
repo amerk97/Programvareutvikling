1bd2b933b9291e9487836638ccebfcc4d6348214
Ellen Zhang Chang <obcellen@gmail.com>
2019-02-26 11:31:50 +0100

    Add ItemMethodTests in tests.py

--- handleliste/shopping_list/tests.py
+++ handleliste/shopping_list/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
+from .models import Item, ShoppingList
 # Create your tests here.
+
+
+class ItemMethodTests(TestCase):
+    self.user = User.objects.create_user(username='testuser', password='12345')
+    login = self.client.login(username='testuser', password='12345')
+
+    def added_item_is_not_bought(self):
+        shopping_list = ShoppingList(
+            title='Desert',
+            owner=self.user
+        )
+        item = Item(
+            name='Sjokolade',
+            amount='10 stk',
+            shopping_list=shopping_list,
+            creator=self.user
+        )
+        self.assertFalse(item.bought)
