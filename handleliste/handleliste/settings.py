ec50814a4ac34aa230fa04d92f6c3d8cddef07ba
haakofli <hakon.fuhre.lia@gmail.com>
2019-02-26 13:40:52 +0100

    layout improvements

--- handleliste/handleliste/settings.py
+++ handleliste/handleliste/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'shopping_list',
+    'crispy_forms',
+    'shopping_list.apps.ShoppinglistConfig',
     'user.apps.UserConfig',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
