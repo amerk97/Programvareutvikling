b2c826733c7064dc702d19e266ced6ff1d6f7dfa
Ellen Zhang Chang <obcellen@gmail.com>
2019-03-11 11:19:55 +0100

    Add admin functionality

--- handleliste/shopping_list/views.py
+++ handleliste/shopping_list/views.py
@@ -12,7 +12,7 @@ from user import views, urls
 User = get_user_model()
 app_name = "shopping_list"
 
-
+# Return the user's shopping lists
 def get_users_shopping_lists(user):
     owned_shopping_lists = ShoppingList.objects.filter(owner=user)
     other_shopping_lists = ShoppingList.objects.filter(participants=user)
@@ -37,7 +37,12 @@ def index(request):
 @login_required(login_url='')
 @require_POST
 def add_item(request, shopping_list_id):
-    shopping_list = ShoppingList.objects.get(pk=shopping_list_id)
+    try:
+        shopping_list = ShoppingList.objects.filter(pk=shopping_list_id)[0]
+    except ShoppingList.DoesNotExist:
+        # The shopping list was deleted while item was added
+        redirect('index')
+
     form = ItemForm(request.POST)
     creator = request.user
 
@@ -59,31 +64,32 @@ def add_item(request, shopping_list_id):
         return redirect('index')
 
 
-def bought_item(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    item.bought = True
-    item.save()
-    shopping_list_id = item.shopping_list.id
+def bought_item(request, item_id, shopping_list_id):
+    try:
+        item = Item.objects.get(pk=item_id)
+        item.bought = True
+        item.save()
+    finally:
+        return redirect('detail', shopping_list_id)
 
-    return redirect('detail', shopping_list_id)
 
-
-def not_bought_item(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    item.bought = False
-    item.save()
-    shopping_list_id = item.shopping_list.id
-
-    return redirect('detail', shopping_list_id)
+def not_bought_item(request, item_id, shopping_list_id):
+    try:
+        item = Item.objects.get(pk=item_id)
+        item.bought = False
+        item.save()
+    finally:
+        return redirect('detail', shopping_list_id)
 
 
 @require_POST
-def delete_item(request, item_id):
-    item = Item.objects.get(pk=item_id)
-    shopping_list_id = item.shopping_list.id
-    item.delete()
-
-    return redirect('detail', shopping_list_id)
+def delete_item(request, item_id, shopping_list_id):
+    try:
+        item = Item.objects.get(pk=item_id)
+        shopping_list_id = item.shopping_list.id
+        item.delete()
+    finally:
+        return redirect('detail', shopping_list_id)
 
 
 @login_required(login_url='')
@@ -132,9 +138,14 @@ def shopping_list_details(request, shopping_list_id):
 
 @login_required(login_url='')
 def delete_shopping_list(request, shopping_list_id):
-    ShoppingList.objects.filter(pk=shopping_list_id).delete()
-
-    return redirect('index')
+    try:
+        shopping_list = ShoppingList.objects.filter(pk=shopping_list_id)
+        if request.user == shopping_list.owner:
+            shopping_list.delete()
+        else:
+            return HttpResponse('Error 401: Unauthorized. User does not have permission to delete shopping list.', status=401)
+    finally:
+        return redirect('index')
 
 
 @login_required(login_url='')
@@ -145,7 +156,6 @@ def share_shopping_list(request, shopping_list_id):
     shopping_list_form = ShoppingListForm()
     item_list = Item.objects.filter(shopping_list=shopping_list_id)
     item_form = ItemForm()
-    user = request.user
     my_shopping_lists = get_users_shopping_lists(user)
 
     if share_form.is_valid():
@@ -169,3 +179,52 @@ def share_shopping_list(request, shopping_list_id):
         return redirect('detail', shopping_list_id)
     else:
         return redirect('index')
+
+
+@login_required(login_url='')
+def remove_user_from_shopping_list(request, shopping_list_id, username):
+    current_user = request.user
+
+    try:
+        shopping_list = ShoppingList.objects.filter(pk=shopping_list_id)[0]
+    except ShoppingList.DoesNotExist:
+        # The shopping list has been deleted while this request was made
+        return redirect('index')
+
+    user = User.objects.get(username=username)
+    try:
+        if user == current_user and user == shopping_list.owner:
+            change_owner_of_shopping_list(shopping_list_id, username)
+        elif user in shopping_list.participants.all():
+            shopping_list.participants.remove(user)
+        elif user in shopping_list.admins.all():
+            shopping_list.admins.remove(user)
+    finally:
+        # If the current user is removed from shopping list, redirect to index
+        # Else redirect to the current shopping list
+        if current_user == user:
+            return redirect('index')
+        return redirect('detail', shopping_list_id)
+
+
+def change_owner_of_shopping_list(request, shopping_list_id, username):
+    shopping_list = ShoppingList.objects.filter(pk=shopping_list_id).update(owner=User(username=username))
+
+
+def make_user_admin_of_shopping_list(request, shopping_list_id, username):
+    try:
+        shopping_list = ShoppingList.objects.filter(pk=shopping_list_id)[0]
+    except ShoppingList.DoesNotExist:
+        # The shopping list was deleted while this request was made
+        return redirect('index')
+
+    user = User.objects.get(username=username)
+
+    try:
+        shopping_list.participants.remove(user)
+        shopping_list.admin.add(user)
+    except:
+        # Something went wrong
+        return redirect('index')
+    finally:
+        return redirect('detail', shopping_list_id)
