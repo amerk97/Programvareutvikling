ed5c3f0e5e6cff5503b3031c7956e4c1eaa198df
Ellen Zhang Chang <obcellen@gmail.com>
2019-03-14 10:06:44 +0100

    Add login and register error feedback

--- handleliste/user/views.py
+++ handleliste/user/views.py
@@ -14,15 +14,12 @@ def register(request):
 		if form.is_valid():
 			user = form.save()
 			username = form.cleaned_data.get('username')
-			messages.success(request, f"New account created: {username}")
 			login(request, user)
-			messages.info(request, f"Logged in as: {username}")
 			return redirect("user:registered")
 		else:
-			for msg in form.error_messages:
-				messages.error(request, f"Oops! {form.error_messages[msg]}")
-				redirect("user:register")
-
+			for msg in form.errors:
+				messages.error(request, f"{form.errors[msg]}")
+				return redirect("user:register")
 
 	form = UserCreationForm()
 	return render(request, "user/register.html", context={"form": form})
@@ -38,27 +35,27 @@ def home(request):
 
 def logout_request(request):
 	logout(request)
-	messages.info(request, "Logged out successfully!")
+	messages.success(request, "Logged out successfully!")
 	return redirect('user:login')
 
 
 def login_request(request):
 
 	if request.method == "POST":
-		form = AuthenticationForm(request, data = request.POST)
+		form = AuthenticationForm(request, data=request.POST)
 		if form.is_valid():
 			username = form.cleaned_data.get("username")
 			password = form.cleaned_data.get("password")
 			user = authenticate(username=username, password=password)
 			if user is not None:
 				login(request, user)
-				messages.success(request, f"Successfully logged in as: {username}")
-
 				return redirect("index")
 			else:
-				messages.error(request, "Invalid username or password")
+				messages.error(request, "Invalid username or password.")
 		else:
-			messages.error(request, "Invalid username or password")
+			messages.error(request, "Invalid username or password.")
+
+		render(request, "user/login.html", {"form": form})
 
 	form = AuthenticationForm()
 
