cf148822ae6b51883c11c7652fdb85c866552789
Damir Medakovic <dmedakovic@vpn-10-52-108-202.vpn-s.ntnu.no>
2019-03-26 12:05:26 +0100

    added autostart for mac

--- auto_start_mac.py
+++ auto_start_mac.py
@@ -29,11 +29,10 @@ try:
     s.sendline('python3 manage.py runserver 0.0.0.0:8000')
     s.prompt()
     print("[+] Django server up and running.")
-
     print("[+] Opening your browser at http://74.207.252.20:8000.")
 
 
-    os.system("xdg-open http://74.207.252.20:8000")
+    os.system("open http://74.207.252.20:8000")
 
     s.logout()
 
